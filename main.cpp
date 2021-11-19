88b1c2fed2ed689737e5b91b7988d5acfc1cea6f
Russell H <rjhua@outlook.com>
2021-11-18 16:35:57 -0800

    removes function and code

--- main.cpp
+++ main.cpp
@@ -1,20 +1,7 @@
 #include <iostream>
 using std::cout;
-void test(int current, int* walker);
 int main() {
-    const int kArr = 10;
-    int* arr = new int[kArr];
-    test(kArr, arr);
-    for (int i = 0; i < kArr; i++) {
-        cout << arr[i] << " ";
-    }
-    delete[] arr;
+
 }
 // test
 // test2
-void test(int current, int* walker) {
-    if (current > 0) {
-        *walker = current;
-        test(current - 1, walker + 1);
-    }
-}
