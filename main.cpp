c8fdfa091e0f777e64113951caf87648c4ae6d87
Russell H <rjhua@outlook.com>
2022-02-13 13:48:53 -0800

    adds files for class and removes text files and adds formatter file

--- main.cpp
+++ main.cpp
@@ -1,31 +1,8 @@
 #include <iostream>
 using std::cout;
-void shiftLeft(int* arr, const int kArr, int location);
-void displayArray(const int* arr, int size);
-int main() {
-    const int kArr = 10;
-    int kArrSize = kArr;
-    int* arr = new int[kArr];
-    for (int i = 0; i < kArr; i++) {
-        arr[i] = i;
-    }
-    displayArray(arr, kArrSize);
-    shiftLeft(arr, kArr, 0);
-    kArrSize--;
-    displayArray(arr, kArrSize);
-    shiftLeft(arr, kArr, kArrSize);
-    kArrSize--;
-    displayArray(arr, kArrSize);
-    return 0;
-}
-void shiftLeft(int* arr, const int kArr, int location) {
-    for (int i = location; i < kArr - 1; i++) {
-        arr[i] = arr[i + 1];
-    }
-}
-void displayArray(const int* arr, int size) {
-    for (int i = 0; i < size; i++) {
-        cout << arr[i] << "|";
-    }
-    cout << "\n";
+
+int main() {}
+void test() {
+    cout << "test\n";
+    cout << "test\n";
 }
---
+++ src/test.h
@@ -0,0 +1,4 @@
+#ifndef SRC_TEST_H_
+#define SRC_TEST_H_
+
+#endif  // SRC_TEST_H_
