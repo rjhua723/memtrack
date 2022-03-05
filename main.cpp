c5ae55f5462d064bbe740727c2bbbc58fc72453f
russelljh <85257210+russelljh@users.noreply.github.com>
2022-03-04 22:46:51 -0800

    mroe tests, not fully completed

--- main.cpp
+++ main.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
-
-#include "./src/test.h"
-using std::cout;
-#include <iostream>
 #include <string>
 #include <vector>
 
+#include "./src/test.h"
+using std::cout;
+
 void test();
-int main() { test(); }
+int main() {
+    test();
+    PrintMemoryUsage();
+}
 void test() {
     const int SIZE = 10;
     PrintMemoryUsage();
@@ -19,4 +21,11 @@ void test() {
     PrintMemoryUsage();
     delete[] test;
     PrintMemoryUsage();
+
+    std::vector<int> vTest = {1, 1, 7, 7, 8};
+    PrintMemoryUsage();
+    for (int i = 0; i < 10; i++) {
+        vTest.push_back(i);
+        PrintMemoryUsage();
+    }
 }
--- src/test.h
+++ src/test.h
@@ -5,7 +5,6 @@
 struct AllocationMetrics {
     uint32_t TotalAllocated = 0;
     uint32_t TotalFreed = 0;
-
     uint32_t CurrentUsage() { return TotalAllocated - TotalFreed; }
 };
 static AllocationMetrics s_AllocationMetrics;
@@ -18,9 +17,19 @@ void* operator new(size_t size) {
     // std::cout << "Allocating " << size " bytes \n";
     return malloc(size);
 }
+/**
+ * @brief this is an overloaded new[] that tracks the allocated size "behind"
+ * the pointer
+ * @note The void* is manipulated with static_cast to different types. For
+ * example, to set the size "behind" the pointer, use a size_t* to set the size
+ * allocated. To offset the pointer, use a char* and offset it by how much was
+ * allocated "behind" the pointer.
+ * @param size The size to allocate. This will be kept track of
+ * @return void* A memory address to allocated memory. At index [-1] the size in bytes should be available.
+ */
 void* operator new[](size_t size) {
     s_AllocationMetrics.TotalAllocated += size;
-    void* p = malloc(size + sizeof(size));
+    void* p = malloc(sizeof(size) + size);
     static_cast<size_t*>(p)[0] = size;
     p = static_cast<char*>(p) + sizeof(size);
     return p;
